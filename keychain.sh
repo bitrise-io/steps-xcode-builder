628387118d91c8b29719b8318e0680562a1e996d
Barnabás Birmacher <birmacher@gmail.com>
2014-04-13 11:16:04 +0200

    keychain improvements

--- keychain.sh
+++ keychain.sh
@@ -1,11 +1,21 @@
-# Create the keychain
-security create-keychain -p $CONCRETE_KEYCHAIN_PASSPHRASE $CONCRETE_KEYCHAIN_PATH
+if [ $1 == "add"]; then
+  export SAVED_KEYCHAINS=$(security list-keychain)
 
-# Import to keychain
-security import $CONCRETE_CERTIFICATE_PATH -k $CONCRETE_KEYCHAIN_PATH -P $CONCRETE_CERTIFICATE_PASSPHRASE -T /usr/bin/codesign
+  # Create the keychain
+  security create-keychain -p $CONCRETE_KEYCHAIN_PASSPHRASE $CONCRETE_KEYCHAIN_PATH
 
-# Add to the searchable keychains
-security list-keychain -s CONCRETE_KEYCHAIN_PATH
+  # Import to keychain
+  security import $CONCRETE_CERTIFICATE_PATH -k $CONCRETE_KEYCHAIN_PATH -P $CONCRETE_CERTIFICATE_PASSPHRASE -T /usr/bin/codesign
 
-# Unlock keychain
-security unlock-keychain -p $CONCRETE_KEYCHAIN_PASSPHRASE $CONCRETE_KEYCHAIN_PATH
+  # Add to the searchable keychains
+  security list-keychain -s $CONCRETE_KEYCHAIN_PATH
+
+  # Unlock keychain
+  security unlock-keychain -p $CONCRETE_KEYCHAIN_PASSPHRASE $CONCRETE_KEYCHAIN_PATH
+elif [ $1 == "remove" ]; then
+  security delete-keychain $CONCRETE_KEYCHAIN_PATH
+
+  unset SAVED_KEYCHAINS
+else
+  # Do nothing
+fi
