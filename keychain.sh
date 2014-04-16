98f30885f9a0b8406075c6fc08ede2c159621f78
Barnabás Birmacher <birmacher@gmail.com>
2014-04-17 00:53:07 +0200

    keychain with unlock changes

--- keychain.sh
+++ keychain.sh
@@ -1,8 +1,9 @@
 if [[ $1 == "add" ]]; then
   export SAVED_KEYCHAINS=$(security list-keychain)
+  export KEYCHAIN_PASSPHRASE=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
   # Create the keychain
-  security create-keychain -p $CONCRETE_KEYCHAIN_PASSPHRASE $CONCRETE_KEYCHAIN_PATH
+  security create-keychain -p $KEYCHAIN_PASSPHRASE $CONCRETE_KEYCHAIN_PATH
 
   # Import to keychain
   security import $CERTIFICATE_PATH -k $CONCRETE_KEYCHAIN_PATH -P $CONCRETE_CERTIFICATE_PASSPHRASE -T /usr/bin/codesign
@@ -11,7 +12,8 @@ if [[ $1 == "add" ]]; then
   security list-keychain -s $CONCRETE_KEYCHAIN_PATH
 
   # Unlock keychain
-  security unlock-keychain -p $CONCRETE_KEYCHAIN_PASSPHRASE $CONCRETE_KEYCHAIN_PATH
+  security unlock-keychain -p $KEYCHAIN_PASSPHRASE $CONCRETE_KEYCHAIN_PATH
+  security set-keychain-settings -lut 7200 $CONCRETE_KEYCHAIN_PATH
 elif [[ $1 = "remove" ]]; then
   security delete-keychain $CONCRETE_KEYCHAIN_PATH
 
