bd1780ed8352e842df19b5d26b97e95f293b9bdd
Viktor <viktor>
2014-04-27 15:33:49 +0200

    more parentheses inclusion

--- keychain.sh
+++ keychain.sh
@@ -3,16 +3,16 @@ if [[ $1 == "add" ]]; then
   export KEYCHAIN_PASSPHRASE="$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
 
   # Create the keychain
-  security -v create-keychain -p $KEYCHAIN_PASSPHRASE $CONCRETE_KEYCHAIN
-  security -v default-keychain -d user -s $CONCRETE_KEYCHAIN
+  security -v create-keychain -p "$KEYCHAIN_PASSPHRASE $CONCRETE_KEYCHAIN"
+  security -v default-keychain -d user -s "$CONCRETE_KEYCHAIN"
 
   # Import to keychain
-  security -v import $CERTIFICATE_PATH -k $CONCRETE_KEYCHAIN -P $CONCRETE_CERTIFICATE_PASSPHRASE -T /usr/bin/codesign
+  security -v import "$CERTIFICATE_PATH" -k "$CONCRETE_KEYCHAIN" -P "$CONCRETE_CERTIFICATE_PASSPHRASE" -T /usr/bin/codesign
 
   # Unlock keychain
-  security -v unlock-keychain -p $KEYCHAIN_PASSPHRASE $CONCRETE_KEYCHAIN
-  security -v set-keychain-settings -lut 7200 $CONCRETE_KEYCHAIN
+  security -v unlock-keychain -p "$KEYCHAIN_PASSPHRASE" "$CONCRETE_KEYCHAIN"
+  security -v set-keychain-settings -lut 7200 "$CONCRETE_KEYCHAIN"
   security -v list-keychains
 elif [[ $1 = "remove" ]]; then
-  security -v delete-keychain $CONCRETE_KEYCHAIN
+  security -v delete-keychain "$CONCRETE_KEYCHAIN"
 fi
