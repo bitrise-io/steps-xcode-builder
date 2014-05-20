352887165f3d900925c5f9a0772c8d9db6af8302
Viktor <viktor>
2014-05-21 00:29:04 +0200

    dsym search string fix

--- step.sh
+++ step.sh
@@ -192,6 +192,8 @@ if [ -n "$CONCRETE_ACTION_ARCHIVE" ]; then
     app_dsym_count=0
     app_dsym_path=""
     for a_app_dsym in "$archive_dsyms_folder/*.app.dSYM"; do
+      # get the file path as a string (by default it's a "query" and enclosing in "" will fail)
+      a_app_dsym=$(printf "%s" $a_app_dsym)
       if [ -d "$a_app_dsym" ]; then
         echo " (i) .app.dSYM found: $a_app_dsym"
         app_dsym_count=$[app_dsym_count + 1]
