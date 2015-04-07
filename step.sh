0cf085aa2c09bbe845539c02587ebd6f740209d1
Viktor Benei <viktor.benei@gmail.com>
2015-04-07 15:00:57 +0200

    space in path #fix

--- step.sh
+++ step.sh
@@ -394,7 +394,7 @@ if [[ "${XCODE_BUILDER_ACTION}" == "archive" ]] ; then
     write_section_to_formatted_output "## Generating signed IPA"
 
     # Get the name of the profile
-    profile_name=`/usr/libexec/PlistBuddy -c 'Print :Name' /dev/stdin <<< $(security cms -D -i ${ARCHIVE_PATH}/Products/Applications/*.app/embedded.mobileprovision)`
+    profile_name=`/usr/libexec/PlistBuddy -c 'Print :Name' /dev/stdin <<< $(security cms -D -i "${ARCHIVE_PATH}/Products/Applications/*.app/embedded.mobileprovision")`
     fail_if_cmd_error "Missing embedded mobileprovision in xcarchive"
 
     echo " (i) Found Profile Name, for signing: ${profile_name}"
